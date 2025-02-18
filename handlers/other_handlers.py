6e63ff5fae5ce02b9aeb2e7f482b39dac192c836
AleksandrUche <zak.reizer@mail.ru>
2025-02-18 23:38:59 +0300

    feat: перенес регистрацию в сервисный слой

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,41 +1,19 @@
-from aiogram import Router, F
+from aiogram import Router
 from aiogram.filters import Command
 from aiogram.filters import CommandStart
-from aiogram.types import Message, CallbackQuery
-from sqlalchemy.exc import IntegrityError
+from aiogram.types import Message
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database.models.user_models import UserOrm
-from keyboards.inline_keyboards import inline_keyboards
 from keyboards.inline_keyboards.inline_keyboards import main_menu_inline
-from keyboards.reply_keyboards import reply_keyboards
 from lexicon.lexicon import LEXICON_RU
+from services.registration.registration import user_registration
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def start_handler(message: Message, session: AsyncSession):
-    user = UserOrm(
-        telegram_id=message.from_user.id,
-        username=message.from_user.username,
-        first_name=message.from_user.first_name,
-        last_name=message.from_user.last_name,
-    )
-    session.add(user)
-    try:
-        await session.commit()
-        await message.answer(
-            text=f'Здравствуйте, {user.first_name}! Вы успешно зарегистрированы!\n'
-                 'Я бот-помощник создан для ухода за вашими питомцами 🦎🐍🦖\n',
-            reply_markup=reply_keyboards.main_menu
-        )
-    except IntegrityError:
-        await message.answer(
-            text=f'Рады вас видеть снова {user.first_name}😊\n'
-                 'Напомню, я бот-помощник,  для ухода за вашими питомцами 🦎🐍🦖\n',
-            reply_markup=inline_keyboards.my_profile,
-        )
+    await user_registration(message, session)
 
 
 @router.message(Command(commands='menu'))
