b3575e6297395fb9cb1816a0f4943cd840dd50fa
AleksandrUche <zak.reizer@mail.ru>
2025-02-19 22:56:30 +0300

    refactor: добавил логи, перенес запрос к бд.

--- handlers/user_handler.py
+++ handlers/user_handler.py
@@ -11,36 +11,20 @@ from keyboards.inline_keyboards import inline_keyboards
 router = Router()
 
 
-@router.message(F.text == 'Профиль')
-async def my_profile(message: Message, session: AsyncSession):
-    user = await session.scalar(
-        select(UserOrm).filter(UserOrm.telegram_id == message.from_user.id)
-    )
-
-    if not user:
-        await message.answer(text='Произошла ошибка, пользователь не найден')
-
-    await message.answer(
-        text=f'Ваше имя: {user.first_name}\n',
-        reply_markup=inline_keyboards.my_profile,
-    )
-
-
-@router.callback_query(F.data == 'about_subscription')
-async def process_buttons_press(callback: CallbackQuery):
-    await callback.message.answer(
-        text='📋Подписка позволяет вам добавлять больше двух питомцев, '
-             'создавать компании/группы, делится ими с другими '
-             'пользователями для администрирования и работы. Выдавать определенную роль '
-             'в компании это может быть как рабочий так и администратор.\n'
-             '🙂 Без подписки вы можете пользоваться ботом, но добавлять больше двух '
-             'питомцев нельзя а так же нельзя создавать дополнительные компании и '
-             'группы животных.\n\n'
-             '⭐Стоимость полного доступа:\n'
-             '...₽ - 30 дней\n'
-             '...₽ - 60 дней\n'
-             '...₽ - 90 дней\n'
-             '...₽ - 183 дня\n'
-             '...₽ - 365 дней\n'
-    )
+@router.callback_query(F.data == 'profile')
+async def my_profile(callback: CallbackQuery, session: AsyncSession):
+    await callback.answer()
+    try:
+        user = await user_exists(callback.from_user.id, session)
+    except Exception as e:
+        logger.error(
+            f'Ошибка при открытии профиля пользователя c id = {callback.from_user.id}: {e}',
+            exc_info=True
+        )
+        await callback.message.answer(text='Произошла ошибка, пользователь не найден')
+    else:
+        await callback.message.answer(
+            text=f'Ваше имя: {user.first_name}\n',
+            reply_markup=inline_keyboards.my_profile,
+        )
     await callback.answer()
