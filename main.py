1cc5f279236a9ef935cb8c35af0a06926f2b0114
AleksandrUche <zak.reizer@mail.ru>
2025-02-19 22:53:22 +0300

    feat: добавлен новый роутер

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from sqlalchemy.testing.suite.test_reflection import users
 
 from config_data.config import BOT_TOKEN
 from database.engine import async_session
+from handlers import user_handler, other_handlers, pet_hendlers
+from keyboards.set_menu import set_main_menu
 from middlewares.db import DataBaseSession
-from handlers import other_handlers
-from handlers import user_handler
-
-from keyboards.set_menu  import set_main_menu
 
 logger = logging.getLogger(__name__)
 
@@ -36,15 +33,14 @@ async def main():
     logger.info('Подключаем роутеры')
     dp.include_router(user_handler.router)
     dp.include_router(other_handlers.router)
-
-
+    dp.include_router(pet_hendlers.router)
 
     logger.info('Подключаем миддлвари')
     dp.update.middleware(DataBaseSession(session_pool=async_session))
 
-
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
