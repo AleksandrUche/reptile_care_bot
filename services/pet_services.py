b34725d207a579633c83c3c48bc6135ee1938839
AleksandrUche <zak.reizer@mail.ru>
2025-02-23 22:16:53 +0300

    feat: добавление питомца

--- services/pet_services.py
+++ services/pet_services.py
@@ -4,7 +4,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
-from database.models.pets_models import CompanyOrm
+from database.models.pets_models import CompanyOrm, GroupOrm, PetOrm
 from database.models.user_models import UserOrm
 
 logger = logging.getLogger(__name__)
@@ -20,13 +20,29 @@ async def get_user_company(user_id: int, session: AsyncSession):
     return company.companies
 
 
-async def get_groups_in_company(user_id: int, session: AsyncSession):
-    """Возвращает все группы связанные с компанией пользователя"""
-    ...
+async def get_company_and_groups(user_id: int, session: AsyncSession):
+    """Возвращает компанию и все группы связанные с ней"""
+    company = await session.scalar(
+        select(CompanyOrm)
+        .options(joinedload(CompanyOrm.groups))
+        .filter(CompanyOrm.user.has(telegram_id=user_id))
+    )
+    return company
 
 
 async def add_pet(user_id: int, pet_name: str, session: AsyncSession):
     """
-    Добавляет питомца в компанию пользователя.
+    Добавляет питомца в компанию пользователя, с группой "По умолчанию".
     """
-    ...
+    user_company = await get_company_and_groups(user_id, session)
+
+    pet = PetOrm(
+        name=pet_name,
+        company_id=user_company.id,
+        group_id=user_company.groups[0].id,
+    )
+    session.add(pet)
+    try:
+        await session.commit()
+    except Exception as e:
+        logger.error(f'Ошибка при добавлении питомца: {e}', exc_info=True)
