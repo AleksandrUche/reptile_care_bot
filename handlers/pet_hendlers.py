256d9b6a05276758f992ccee9e7010e31c2923a8
AleksandrUche <zak.reizer@mail.ru>
2025-02-24 23:07:17 +0300

    feat: добавил handler для отображения всех питомцев

--- handlers/pet_hendlers.py
+++ handlers/pet_hendlers.py
@@ -7,8 +7,9 @@ from aiogram.fsm.state import default_state
 from aiogram.types import CallbackQuery, Message
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from filters.pet_filters import is_alnum_with_spaces
 from keyboards.inline_keyboards import inline_keyboards
-from services.pet_services import get_user_company, get_company_and_groups, add_pet
+from services.pet_services import get_user_company, add_pet, get_my_companies_and_pets
 from states.pet_states import AddPetFSM
 
 logger = logging.getLogger(__name__)
@@ -58,8 +59,38 @@ async def process_pet_name(message: Message, state: FSMContext, session: AsyncSe
     await state.clear()
 
 
-@router.callback_query(F.data == 'cancel_add_pet', ~StateFilter(default_state))
-async def cancel_handler(message: Message, state: FSMContext):
+@router.message(StateFilter(AddPetFSM.pet_name))
+async def warning_incorrect_pet_name(message: Message):
+    """Сработает при некорректном вводе имени питомца"""
+    await message.answer(
+        text='То, что Вы отправили не похоже на имя\n'
+             'Пожалуйста, введите имя еще раз\n'
+             'Имя может состоять из букв и цифр❗'
+    )
+
+
+@router.callback_query(F.data == 'my_pets_list', StateFilter(default_state))
+async def add_pet_handler(callback: CallbackQuery, session: AsyncSession):
+    """Просмотр всех питомцев"""
+    await callback.answer()
+    pets = await get_my_companies_and_pets(callback.from_user.id, session)
+
+    result = []
+    for company in pets:
+        result.append(f'{company.name}:\n\n')
+        for pet in company.pets:
+            result.append(f'- id: {pet.id} Имя: {pet.name}\n')
+        result.append('\n')
+
+    await callback.message.edit_text(
+        text=f'🦎<b>Мои питомцы</b>\n\n' + ''.join(result),
+        reply_markup=inline_keyboards.back_to_main_menu,
+    )
+
+
+@router.callback_query(F.data == 'cancel_state', ~StateFilter(default_state))
+async def cancel_state_handler(message: Message, state: FSMContext):
+    """Выходит из машины состояния"""
     await state.clear()
     await message.answer("Действие отменено.")
 
@@ -70,9 +101,10 @@ async def cancel_handler(message: Message, state: FSMContext):
 async def company_main_menu(callback: CallbackQuery):
     await callback.answer()
     await  callback.message.edit_text(
-        text='Компании 🏙',
+        text='Компании 🏢',
         reply_markup=inline_keyboards.menu_company,
     )
+
 
 @router.callback_query(
     F.data == 'my_companies', StateFilter(default_state)
@@ -80,11 +112,10 @@ async def company_main_menu(callback: CallbackQuery):
 async def my_company(callback: CallbackQuery, session: AsyncSession):
     await callback.answer()
     my_company = await get_user_company(callback.from_user.id, session)
-    companies = ['Все компании: 🏙']
 
+    companies = []
     for company in my_company:
         companies.append(company.name)
 
     await  callback.message.edit_text(
-        text= '\n'.join(companies),
-    )
+        text='Все компании: 🏢\n\n' + '\n'.join(companies), )
