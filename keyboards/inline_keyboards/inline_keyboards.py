b164b0b3750bfd5fb10f849119c4db54fb839713
AleksandrUche <zak.reizer@mail.ru>
2025-02-24 22:57:25 +0300

    refactor: поправлены клавиатуры

--- keyboards/inline_keyboards/inline_keyboards.py
+++ keyboards/inline_keyboards/inline_keyboards.py
@@ -1,17 +1,10 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
 from keyboards.keyboard_utils.inline_kb_utils import create_inline_kb
 
-my_profile = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text='О боте', callback_data='about_bot'),
-         InlineKeyboardButton(text='О подписке', callback_data='about_subscription')],
-        [InlineKeyboardButton(text='История пополнений',
-                              callback_data='payment_history'),
-         InlineKeyboardButton(text='Добавить компанию', callback_data='add_company')],
-        [InlineKeyboardButton(text='Пользовательское соглашение',
-                              callback_data='user_offer')],
-    ],
+my_profile = create_inline_kb(
+    1,
+    about_subscription='Подписка',
+    payment_history='История пополнений',
+    back_to_main_menu='⬅ Назад',
 )
 
 main_menu_inline = create_inline_kb(
@@ -21,17 +14,27 @@ main_menu_inline = create_inline_kb(
     pets='Кормления',
     profile='Профиль',
     supports='Поддержка',
+    about_bot='О боте',
+    user_offer='Пользовательское соглашение',
 )
 
 main_menu_pets = create_inline_kb(
     2,
     add_pet='➕ Добавить питомца',
-    my_pets='🧾 Мои питомцы',
+    my_pets_list='🧾 Мои питомцы',
     back_to_main_menu='⬅ Назад',
 )
 
 menu_add_pet = create_inline_kb(
-    2, cancel_add_pet='Отмена', back_to_pets_menu='⬅ Назад'
+    2, cancel_state='Отмена', back_to_pets_menu='⬅ Назад'
 )
 
-menu_company = create_inline_kb(2, my_companies='Мои компании')
+back_to_main_menu = create_inline_kb(
+    1, back_to_main_menu='⬅ Назад'
+)
+
+menu_company = create_inline_kb(
+    1,
+    my_companies='Мои компании',
+    back_to_main_menu='⬅ Назад',
+)
