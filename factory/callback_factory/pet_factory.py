b15dd4349d49582561e61e9f785e80a0e045430b
AleksandrUche <zak.reizer@mail.ru>
2025-02-27 23:00:39 +0300

    feat: добавлена фабрика для обработки действий с питомцем

--- factory/callback_factory/pet_factory.py
+++ factory/callback_factory/pet_factory.py
@@ -10,3 +10,8 @@ class PetsCallbackFactory(CallbackData, prefix='pet'):
     id: int
     company_id: int
     group_id: int
+
+
+class EditPetCallback(CallbackData, prefix='edit_pet'):
+    field: str  # Поле, которое нужно изменить (например, "name", "morph"...)
+    pet_id: int
