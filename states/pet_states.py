43e404511ac95e239e1485a0873593ff732b33ab
AleksandrUche <zak.reizer@mail.ru>
2025-03-02 22:30:26 +0300

    refactor: добавлены FSM

--- states/pet_states.py
+++ states/pet_states.py
@@ -1,5 +1,55 @@
 from aiogram.fsm.state import State, StatesGroup
 
 
-class AddPetFSM(StatesGroup):
+class PetAddFSM(StatesGroup):
     pet_name = State()
+
+
+class PetIdFSM(StatesGroup):
+    """Для добавления поля id питомца"""
+    pet_id = State()
+
+
+class PetEditNameFSM(PetIdFSM):
+    """Имя питомца"""
+    pet_name = State()
+
+
+class PetEditMorphFSM(PetIdFSM):
+    """Морфа питомца"""
+    pet_morph = State()
+
+
+class PetEditViewFSM(PetIdFSM):
+    """Вид питомца"""
+    pet_view = State()
+
+
+class PetEditGenderFSM(PetIdFSM):
+    """Пол питомца"""
+    pet_gender = State()
+
+
+class PetEditLengthFSM(PetIdFSM):
+    """Длина питомца"""
+    pet_length = State()
+
+
+class PetEditMoltingFSM(PetIdFSM):
+    """Линька питомца"""
+    pet_ = State()
+
+
+class PetEditBirthFSM(PetIdFSM):
+    """Дата рождения"""
+    pet_birth = State()
+
+
+class PetEditPurchaseFSM(PetIdFSM):
+    """Дата приобретения"""
+    pet_purchase = State()
+
+
+class PetDeliteFSM(PetIdFSM):
+    """Удаление питомца"""
+    ...
