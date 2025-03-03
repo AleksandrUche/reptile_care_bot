c4e8a4f5bc95b93a47b45a9f37d0765c797ba082
AleksandrUche <zak.reizer@mail.ru>
2025-03-03 21:24:31 +0300

    feat: добавлена фабрика для изменения пола питомца

--- factory/callback_factory/pet_factory.py
+++ factory/callback_factory/pet_factory.py
@@ -1,5 +1,7 @@
 from aiogram.filters.callback_data import CallbackData
 
+from enums.pets_enum import GenderRole
+
 
 class PaginationCallback(CallbackData, prefix='paginate'):
     action: str  # Действие: 'prev' или 'next'
@@ -7,7 +9,7 @@ class PaginationCallback(CallbackData, prefix='paginate'):
 
 
 class PetsCallback(CallbackData, prefix='pet'):
-    id: int
+    pet_id: int
     company_id: int
     group_id: int
 
@@ -15,9 +17,18 @@ class PetsCallback(CallbackData, prefix='pet'):
 class EditPetCallback(CallbackData, prefix='edit_pet'):
     field: str  # Поле, которое нужно изменить (например, "name", "morph"...)
     pet_id: int
+    company_id: int  # для возврата к детальному просмотру питомца
+    group_id: int
 
 
 class DeletePetCallback(CallbackData, prefix='delete_pet'):
     action: str # menu, delete, cancel
     pet_id: int
     pet_name: str
+
+
+class GenderSelectionCallback(CallbackData, prefix='gender_pet'):
+    action: GenderRole
+    pet_id: int
+    company_id: int # для возврата к детальному просмотру питомца
+    group_id: int
