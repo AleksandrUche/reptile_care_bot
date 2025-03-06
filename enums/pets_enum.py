5e9e13f14ccf75bac53ab85d1c7ee0acb6a193b2
AleksandrUche <zak.reizer@mail.ru>
2025-03-06 22:43:49 +0300

    feat: добавлен enum для единиц измерения

--- enums/pets_enum.py
+++ enums/pets_enum.py
@@ -5,3 +5,11 @@ class GenderRole(Enum):
     BOY = 'мальчик'
     GIRL = 'девочка'
     NOT_DEFINED = 'не определен'
+
+
+class MeasureUnitFood(Enum):
+    """Единицы измерения продуктов"""
+    GRAM = 'г'
+    KILOGRAM = 'кг'
+    QUANTITY = 'шт.'
+    MISS = ''
