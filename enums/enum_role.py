fbfe12eb780441ea7383e4d3a6dd56ea730b1f11
AleksandrUche <zak.reizer@mail.ru>
2025-03-09 15:32:08 +0300

    feat: добавил выбор языка

--- enums/enum_role.py
+++ enums/enum_role.py
@@ -2,20 +2,25 @@ from enum import Enum
 
 
 class UserRole(Enum):
-    OWNER = "owner"
-    ADMIN = "admin"
-    USER = "user"
+    OWNER = 'owner'
+    ADMIN = 'admin'
+    USER = 'user'
 
 
 class SubscriptionType(Enum):
-    ONE_MONTH = "one month"
-    TWO_MONTHS = "two months"
-    SIX_MONTHS = "six months"
+    ONE_MONTH = 'one month'
+    TWO_MONTHS = 'two months'
+    SIX_MONTHS = 'six months'
     YEAR = 'year'
 
 
 class UserRoleCompany(Enum):
-    OWNER = "owner"
-    ADMIN = "admin"
+    OWNER = 'owner'
+    ADMIN = 'admin'
     EDITOR = 'editor'
     VIEWER = 'viewer'
+
+
+class Language(Enum):
+    EN = 'en'
+    RU = 'ru'
