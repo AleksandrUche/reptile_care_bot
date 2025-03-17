498e6661c73b4195891eda01b9035b75ffb46753
Александр Учватов <95583450+AleksandrUche@users.noreply.github.com>
2025-03-17 20:55:47 +0300

    2/Добавление тайм зоны пользователя по геолокации (#2)
    * feat(2): Добавлены обработчики для изменения тайм зоны пользователя по геопозиции
    
    * feat(2): добавлена клавиатура для подтверждения таймзоны по геопозиции
    
    * feat(2): добавлен fsm

--- states/user_states.py
+++ states/user_states.py
@@ -3,3 +3,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 class SearchTimeZoneByCityFSM(StatesGroup):
     city = State()
+
+
+class SearchTimeZoneByGeoPositionFSM(StatesGroup):
+    location = State()
