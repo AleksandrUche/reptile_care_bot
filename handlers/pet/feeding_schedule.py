ba374ca72e10053498867f90079f94fc7f6a5d4b
AleksandrUche <zak.reizer@mail.ru>
2025-04-01 21:36:34 +0300

    feat(3): Добавлен обработчик для группового добавления дат кормлений с описаниями

--- handlers/pet/feeding_schedule.py
+++ handlers/pet/feeding_schedule.py
@@ -8,19 +8,26 @@ from aiogram.types import CallbackQuery, Message
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from factory.callback_factory.pet_factory import (
-    EditPetCallback,
     AddSheduleFeedingsCallback,
 )
 from keyboards.keyboard_utils.inline_kb_utils import (
-    get_return_detail_view_pet_inline_kb,
     get_add_shedule_feedings_inline_kb,
     get_select_shedule_feedings_clear_state_inline_kb,
+    get_select_shedule_feedings_inline_kb,
 )
-from services.pet_services import add_feeding_shedule, time_zone_is_not_set, \
-    add_group_feeding_shedule
+from services.pet_services import (
+    add_feeding_shedule,
+    time_zone_is_not_set,
+    add_group_feeding_shedule,
+    add_group_feeding_and_description_shedule,
+)
 from services.registration_services import get_user
 from services.utils import parse_date, parse_time
-from states.pet_states import FeedingSingleFSM, FeedingGroupFSM
+from states.pet_states import (
+    FeedingSingleFSM,
+    FeedingGroupFSM,
+    FeedingGroupAndDescriptionFSM,
+)
 
 logger = logging.getLogger(__name__)
 router = Router(name='feeding_shedule_pet')
@@ -49,7 +56,9 @@ async def menu_feeding_schedule_handler(
 
 @router.callback_query(AddSheduleFeedingsCallback.filter(F.action == 'single_addition'))
 async def add_single_feeding_schedule_handler(
-    callback: CallbackQuery, callback_data: EditPetCallback, state: FSMContext,
+    callback: CallbackQuery,
+    callback_data: AddSheduleFeedingsCallback,
+    state: FSMContext,
 ):
     """Обработчик для добавления одной даты кормления"""
     await callback.answer()
@@ -122,7 +131,7 @@ async def process_add_single_time_feeding(
         add_feeding = await add_feeding_shedule(
             state_data['pet_id'], date_time, session,
         )
-        inline_back_kb = await get_return_detail_view_pet_inline_kb(
+        inline_back_kb = await get_select_shedule_feedings_inline_kb(
             state_data['pet_id'], state_data['company_id'], state_data['group_id']
         )
         if add_feeding:
@@ -143,7 +152,9 @@ async def process_add_single_time_feeding(
 
 @router.callback_query(AddSheduleFeedingsCallback.filter(F.action == 'group_addition'))
 async def add_group_feedings_schedule_handler(
-    callback: CallbackQuery, callback_data: EditPetCallback, state: FSMContext,
+    callback: CallbackQuery,
+    callback_data: AddSheduleFeedingsCallback,
+    state: FSMContext,
 ):
     """
     Обработчик для добавления группы дат кормлений
@@ -275,7 +286,7 @@ async def process_add_group_feeding_repeat(
     except ValueError:
         await message.answer('Произошла ошибка, пожалуйста, повторите еще раз.')
     else:
-        inline_back_kb = await get_select_shedule_feedings_clear_state_inline_kb(
+        inline_back_kb = await get_select_shedule_feedings_inline_kb(
             state_data['pet_id'], state_data['company_id'], state_data['group_id']
         )
         await message.answer(
@@ -290,5 +301,296 @@ async def warning_incorrect_group_repeat_feeding(message: Message):
     """Сработает при некорректном вводе повторений кормлений"""
     await message.answer(
         text='То, что Вы отправили не похоже на количество повторений кормления.\n'
+             'Пожалуйста, повторите еще раз, допускаются только цифры❗'
+    )
+
+
+@router.callback_query(AddSheduleFeedingsCallback.filter(
+    F.action == 'group_addition_and_description')
+)
+async def add_group_feedings_schedule_with_description_handler(
+    callback: CallbackQuery,
+    callback_data: AddSheduleFeedingsCallback,
+    state: FSMContext,
+):
+    """
+    Обработчик для добавления группы дат кормлений
+    Принимает date, time, offset, repeat,
+    """
+    await callback.answer()
+    inline_kb = await get_select_shedule_feedings_clear_state_inline_kb(
+        callback_data.pet_id, callback_data.company_id, callback_data.group_id
+    )
+    await  callback.message.edit_text(
+        text='Добавление графика кормлений с описаниям.\n\n'
+             '🔙Для возврата нажмите «Отмена», затем «Назад».\n'
+             '<b>Введите дату в формате ДД.ММ.ГГГГ или ДД.ММ.ГГ.</b>\n'
+             'Разделитель может быть: ".", ",", "пробел" и "/".\n',
+        reply_markup=inline_kb
+    )
+    await state.update_data(
+        pet_id=callback_data.pet_id,
+        company_id=callback_data.company_id,
+        group_id=callback_data.group_id
+    )
+    await state.set_state(FeedingGroupAndDescriptionFSM.date)
+
+
+@router.message(StateFilter(FeedingGroupAndDescriptionFSM.date))
+async def process_add_start_date_group_feeding_with_description(
+    message: Message, state: FSMContext, session: AsyncSession
+):
+    """Добавление даты кормлений для добавления группы кормлений с описаниями."""
+    user = await get_user(message.from_user.id, session)
+    user_timezone = tzinfo(user.tz_region)
+
+    if not user_timezone:
+        await time_zone_is_not_set(message, state)
+        return
+
+    try:
+        date = parse_date(message.text)
+        date_feeding = date.replace(tzinfo=user_timezone).date()
+        await state.update_data(date=date_feeding)
+    except ValueError:
+        await message.answer(
+            'Неверный формат даты.\n Введите дату в формате ДД.ММ.ГГГГ или ДД.ММ.ГГ.'
+        )
+    else:
+        state_data = await state.get_data()
+        inline_back_kb = await get_select_shedule_feedings_clear_state_inline_kb(
+            state_data['pet_id'], state_data['company_id'], state_data['group_id']
+        )
+        await message.answer(
+            text='Введите время в формате ЧЧ:ММ.\n'
+                 '🔙Для возврата нажмите «Отмена», затем «Назад».\n\n'
+                 'Разделитель может быть: ":", ".", ",", "пробел" и "/".',
+            reply_markup=inline_back_kb
+        )
+        await state.set_state(FeedingGroupAndDescriptionFSM.time)
+
+
+@router.message(StateFilter(FeedingGroupAndDescriptionFSM.time))
+async def process_add_time_group_feeding_with_description(
+    message: Message, state: FSMContext
+):
+    """Добавление времени кормления."""
+    try:
+        time_feeding = parse_time(message.text)
+        await state.update_data(time=time_feeding)
+    except ValueError:
+        await message.answer('Неверный формат времени. Введите время в формате ЧЧ:ММ.')
+    else:
+        state_data = await state.get_data()
+        inline_back_kb = await get_select_shedule_feedings_clear_state_inline_kb(
+            state_data['pet_id'], state_data['company_id'], state_data['group_id']
+        )
+        await message.answer(
+            text='Введите, через сколько дней будет повторное кормление.\n\n'
+                 'Например: если начало графика 01.04.25, задаем повторное кормление '
+                 'на 4-й день, значит, повторное кормление будет 05.04.25 (кормление '
+                 'на 4-й день, значит, три дня голодовки).\n\n'
+                 '🔙Для возврата нажмите «Отмена», затем «Назад».\n',
+            reply_markup=inline_back_kb,
+        )
+        await state.set_state(FeedingGroupAndDescriptionFSM.offset)
+
+
+@router.message(StateFilter(FeedingGroupAndDescriptionFSM.offset), F.text.isdigit())
+async def process_add_offset_group_feeding_with_description(
+    message: Message, state: FSMContext
+):
+    """Добавление интервала кормлений при групповом добавлении."""
+    try:
+        await state.update_data(offset=message.text)
+    except ValueError:
+        await message.answer('Произошла ошибка, повторите еще раз.')
+    else:
+        state_data = await state.get_data()
+        inline_back_kb = await get_select_shedule_feedings_clear_state_inline_kb(
+            state_data['pet_id'], state_data['company_id'], state_data['group_id']
+        )
+        await message.answer(
+            text='Введите количество повторений.\n\n'
+                 'В расписание будет внесено указанное количество дат '
+                 'c ранее указанным интервалом.\n'
+                 '🔙Для возврата нажмите «Отмена», затем «Назад».\n',
+            reply_markup=inline_back_kb,
+        )
+        await state.set_state(FeedingGroupAndDescriptionFSM.repeat)
+
+
+@router.message(StateFilter(FeedingGroupAndDescriptionFSM.offset))
+async def warning_incorrect_offset_group_feeding_with_description(message: Message):
+    """Сработает при некорректном вводе интервала кормления"""
+    await message.answer(
+        text='То, что Вы отправили не похоже на интервал кормлений.\n'
+             'Пожалуйста, введите интервал еще раз, он может состоять только из цифр❗'
+    )
+
+
+@router.message(StateFilter(FeedingGroupAndDescriptionFSM.repeat), F.text.isdigit())
+async def process_add_repeat_group_feeding_with_description(
+    message: Message, state: FSMContext
+):
+    """Добавление повторения кормлений при групповом добавлении."""
+    try:
+        await state.update_data(repeat=message.text)
+    except ValueError:
+        await message.answer('Произошла ошибка, пожалуйста, повторите еще раз.')
+    else:
+        state_data = await state.get_data()
+        inline_back_kb = await get_select_shedule_feedings_clear_state_inline_kb(
+            state_data['pet_id'], state_data['company_id'], state_data['group_id']
+        )
+        await message.answer(
+            text='Напишите первое описание для кормлений, на следующем шаге Вы укажете '
+                 'количество повторений данного описания.\n\n'
+                 'Например, Вам необходимо придерживаться графика кормлений: три раза '
+                 'подряд с добавкой «кальций» и, например, один с «витаминами» и т.д. '
+                 'на данном шаге указываем «с кальцием»\n\n'
+                 'Добавление второго описания и его повторение будет через один шаг\n',
+            reply_markup=inline_back_kb,
+        )
+        await state.set_state(FeedingGroupAndDescriptionFSM.description_1)
+
+
+@router.message(StateFilter(FeedingGroupAndDescriptionFSM.repeat))
+async def warning_incorrect_repeat_group_feeding_with_description(message: Message):
+    """Сработает при некорректном вводе повторений кормлений"""
+    await message.answer(
+        text='То, что Вы отправили не похоже на количество повторений кормления.\n'
+             'Пожалуйста, повторите еще раз, допускаются только цифры❗'
+    )
+
+
+@router.message(StateFilter(FeedingGroupAndDescriptionFSM.description_1))
+async def process_add_description_1_group_feeding_with_description(
+    message: Message, state: FSMContext
+):
+    """Добавление первого описания кормлений при групповом добавлении с описанием."""
+    try:
+        await state.update_data(description_1=message.text)
+    except ValueError:
+        await message.answer('Произошла ошибка, пожалуйста, повторите еще раз.')
+    else:
+        state_data = await state.get_data()
+        inline_back_kb = await get_select_shedule_feedings_clear_state_inline_kb(
+            state_data['pet_id'], state_data['company_id'], state_data['group_id']
+        )
+        await message.answer(
+            text='Укажите количество повторений первого описания.',
+            reply_markup=inline_back_kb,
+        )
+        await state.set_state(FeedingGroupAndDescriptionFSM.repeat_description_1)
+
+
+@router.message(
+    StateFilter(FeedingGroupAndDescriptionFSM.repeat_description_1),
+    F.text.isdigit()
+)
+async def process_add_repeat_description_1_group_feeding_with_description(
+    message: Message, state: FSMContext
+):
+    """Добавление повторений первого описания при групповом добавлении с описанием."""
+    try:
+        await state.update_data(repeat_description_1=message.text)
+    except ValueError:
+        await message.answer('Произошла ошибка, пожалуйста, повторите еще раз.')
+    else:
+        state_data = await state.get_data()
+        inline_back_kb = await get_select_shedule_feedings_clear_state_inline_kb(
+            state_data['pet_id'], state_data['company_id'], state_data['group_id']
+        )
+        await message.answer(
+            text='Напишите второе описание для кормлений, на следующем шаге Вы укажете '
+                 'количество повторений данного описания.\n\n'
+                 'Например, Вам необходимо придерживаться графика кормлений: три раза '
+                 'подряд с добавкой «кальций» и, например, один с «витаминами» и т.д. '
+                 'на данном шаге указываем «с витаминами»\n\n',
+            reply_markup=inline_back_kb,
+        )
+        await state.set_state(FeedingGroupAndDescriptionFSM.description_2)
+
+
+@router.message(StateFilter(FeedingGroupAndDescriptionFSM.repeat_description_1))
+async def warning_incorrect_repeat_description_1_group_feeding_with_description(
+    message: Message
+):
+    """Сработает при некорректном вводе повторений первого описания"""
+    await message.answer(
+        text='То, что Вы отправили не похоже на количество повторений первого описания.\n'
+             'Пожалуйста, повторите еще раз, допускаются только цифры❗'
+    )
+
+
+@router.message(StateFilter(FeedingGroupAndDescriptionFSM.description_2))
+async def process_add_description_2_group_feeding_with_description(
+    message: Message, state: FSMContext
+):
+    """Добавление второго описания кормлений при групповом добавлении с описанием."""
+    try:
+        await state.update_data(description_2=message.text)
+    except ValueError:
+        await message.answer('Произошла ошибка, пожалуйста, повторите еще раз.')
+    else:
+        state_data = await state.get_data()
+        inline_back_kb = await get_select_shedule_feedings_clear_state_inline_kb(
+            state_data['pet_id'], state_data['company_id'], state_data['group_id']
+        )
+        await message.answer(
+            text='Укажите количество повторений второго описания.',
+            reply_markup=inline_back_kb,
+        )
+        await state.set_state(FeedingGroupAndDescriptionFSM.repeat_description_2)
+
+
+@router.message(
+    StateFilter(FeedingGroupAndDescriptionFSM.repeat_description_2),
+    F.text.isdigit()
+)
+async def process_add_repeat_description_2_group_feeding_with_description(
+    message: Message, state: FSMContext, session: AsyncSession
+):
+    """
+    Добавление повторений второго описания при групповом добавлении с описанием и
+    финишная работа со всеми данными.
+    """
+    try:
+        await state.update_data(repeat_description_2=message.text)
+        state_data = await state.get_data()
+        date_time = datetime.combine(state_data['date'], state_data['time'])
+        await add_group_feeding_and_description_shedule(
+            state_data['pet_id'],
+            date_time,
+            int(state_data['offset']),
+            int(state_data['repeat']),
+            state_data['description_1'],
+            int(state_data['repeat_description_1']),
+            state_data['description_2'],
+            int(state_data['repeat_description_2']),
+            session,
+        )
+    except ValueError:
+        await message.answer('Произошла ошибка, пожалуйста, повторите еще раз.')
+    else:
+        state_data = await state.get_data()
+        inline_back_kb = await get_select_shedule_feedings_inline_kb(
+            state_data['pet_id'], state_data['company_id'], state_data['group_id']
+        )
+        await message.answer(
+            text='График кормлений с описаниями успешно добавлен ✅',
+            reply_markup=inline_back_kb,
+        )
+        await state.clear()
+
+
+@router.message(StateFilter(FeedingGroupAndDescriptionFSM.repeat_description_2))
+async def warning_incorrect_repeat_description_2_group_feeding_with_description(
+    message: Message
+):
+    """Сработает при некорректном вводе повторений второго описания"""
+    await message.answer(
+        text='То, что Вы отправили не похоже на количество повторений второго описания.\n'
              'Пожалуйста, повторите еще раз, допускаются только цифры❗'
     )
