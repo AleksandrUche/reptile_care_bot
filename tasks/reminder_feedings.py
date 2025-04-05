e2458faac925e7279a9c262145409c3cdc33a9d2
AleksandrUche <zak.reizer@mail.ru>
2025-04-05 17:37:35 +0300

    feat(3): Добавлена клава и обработка времени по таймзоне (взаимодействие с уведомлениями)

--- tasks/reminder_feedings.py
+++ tasks/reminder_feedings.py
@@ -1,10 +1,12 @@
 import asyncio
 import logging
-from datetime import time
+from datetime import time, datetime, timezone
 from typing import Optional
+from zoneinfo import ZoneInfo
 
 from aiogram import Bot
 from aiogram.exceptions import TelegramAPIError
+from saq.types import Context
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
@@ -13,7 +15,9 @@ from config_data.config import BOT_TOKEN
 from database.engine import async_session
 from database.models.pets_models import CompanyOrm, PetOrm, FeedingScheduleOrm
 from database.models.user_models import UserOrm
-from keyboards.inline_keyboards.inline_keyboards import shedule_feeding_approve
+from keyboards.keyboard_utils.inline_kb_utils import (
+    get_shedule_feeding_approve_inline_kb,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -31,10 +35,11 @@ async def _get_feedings_reminder(session: AsyncSession) -> Optional[list]:
                 .joinedload(PetOrm.company)
                 .joinedload(CompanyOrm.user)
                 .load_only(
-                    UserOrm.telegram_id,
+                    UserOrm.telegram_id, UserOrm.tz_region,
                 )
             )
             .filter(
+                FeedingScheduleOrm.scheduled_time <= datetime.now(timezone.utc),
                 FeedingScheduleOrm.is_active == False,
                 FeedingScheduleOrm.remind == True,
             )
@@ -53,13 +58,24 @@ async def _send_notification_safe(bot, feeding: FeedingScheduleOrm):
     """Безопасная отправка уведомления с повторными попытками"""
     for _ in range(2):
         try:
+            date_time = feeding.scheduled_time
+            shedule_time = date_time.astimezone(
+                ZoneInfo(feeding.pet.company.user.tz_region)
+            ).strftime('%d.%m.%Y, %H:%M')
+
+            inline_kb = await get_shedule_feeding_approve_inline_kb(
+                feeding.id, feeding.pet.id, feeding.pet.name
+            )
+
             await bot.send_message(
                 chat_id=feeding.pet.company.user.telegram_id,
                 text=f'⏰ Напоминаю, пора покормить {feeding.pet.name}!\n'
                      f'Вид: {feeding.pet.view}\n'
                      f'Морфа: {feeding.pet.morph}\n'
-                     f'Пол: {feeding.pet.gender.value}\n',
-                reply_markup=shedule_feeding_approve,
+                     f'Описание к кормлению: {feeding.description}\n'
+                     f'Пол питомца: {feeding.pet.gender.value}\n'
+                     f'Дата кормления: {shedule_time}',
+                reply_markup=inline_kb,
             )
             return
         except TelegramAPIError as e:
@@ -72,7 +88,7 @@ async def _send_notification_safe(bot, feeding: FeedingScheduleOrm):
     raise TelegramAPIError("Не удалось отправить сообщение после 2 попыток")
 
 
-async def run_reminder_of_feedings(ctx):
+async def run_reminder_of_feedings(ctx: Context):
     """
     Проверяет расписание кормлений, у которых пользователь поставил
     отметку «напомнить позже», и отправляет уведомление.
@@ -101,3 +117,14 @@ async def run_reminder_of_feedings(ctx):
             logger.info(
                 f'Не удалось найти запланированные кормления: {e}', exc_info=True
             )
+
+if __name__ == '__main__':
+    # Тестовый запуск TODO убрать, добавить в тестирование
+    async def test_func():
+        async with async_session() as session:
+            aa = await _get_feedings_reminder(session)
+            bot = Bot(token=BOT_TOKEN)
+            await _send_notification_safe(bot, aa[0])
+
+
+    asyncio.run(test_func())
