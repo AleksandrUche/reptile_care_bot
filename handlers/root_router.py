95cb393d9b176963d8e456b9791eda3613d61421
AleksandrUche <zak.reizer@mail.ru>
2025-04-13 17:44:29 +0300

    refactor(12): Добавлены роутеры

--- handlers/root_router.py
+++ handlers/root_router.py
@@ -1,6 +1,10 @@
 from aiogram import Router
 from .pet import pet_router
+from .user import user_router
+from .other import other_router
 
 main_router = Router()
 
 main_router.include_router(pet_router)
+main_router.include_router(user_router)
+main_router.include_router(other_router)
